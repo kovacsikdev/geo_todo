b3f4464670e5cf67bfc963564efbf0b17d44f268
Michael Kovacsik <kovacsikdev@gmail.com>
2026-05-15 10:02:05 -0700

    update manifest and icon file names

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header id="Header">
       <div className="header-title">
-        <img src="/icon-128.webp" alt="Map itinerary logo" />
+        <img src="/icon_128.webp" alt="Map itinerary logo" />
         <h1 className="color-primary">MapItin</h1>
       </div>
       <button className="cta-button" onClick={() => navigate("/app")}>
